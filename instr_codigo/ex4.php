442bee7070473fca156d0a6d26654461df7fd9c7
TaianeM <taianemedeiro16@gmail.com>
2024-06-27 23:52:47 -0400

    instruções de cod

---
+++ instr_codigo/ex4.php
@@ -0,0 +1,15 @@
+<?php
+
+/* As instruções simples de PHP são separadas por ponto e vírgula;
+Instruções simples são instruções de uma linha;
+Quando há uma instrução maior, como de condição ou repetição, a definição da mesma é
+dada por abertura e fechamento de chaves;
+Nestes casos não precisaremos de ponto e vírgula;
+*/
+
+echo "One line <br>";
+echo "Another line <br>";
+
+if (5 > 2) {
+    echo "5 e maior que 2";
+}
